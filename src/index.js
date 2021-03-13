0fa393e20ddc1742c3d12a3324fe972f6ddf7e89
mrStarSick <for_shady@mail.ru>
2021-03-13 13:08:55 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    let newArr = [];
+
+    if (str.length % 2 != 0) {
+        return false;
+
+    }
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        newArr.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+    for (let i = 0; i < newArr.length; i++) {
+        if (str.includes(newArr[i])) {
+            str = str.replace(newArr[i], "");
+            i = -1;
+        }
+    }
+    if (str.length != 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
